3eaba385c3746599abbfd5ac4e790fa3ca6332bc
Wave52 <7003397@qq.com>
2025-06-11 21:22:14 +0800

    chore: set deploy env

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,5 +1,8 @@
 // const BASE_URL = 'http://192.168.50.228:9416';
-const BASE_URL = 'https://lets-pray-qa.zeabur.app';
+
+const DEPLOY_ENV = import.meta.env.PUBLIC_DEPLOY_ENV || 'prod';
+
+const BASE_URL = DEPLOY_ENV === 'prod' ? 'https://api.auraofhope.com' : 'https://lets-pray-qa.zeabur.app';
 
 export const decodeShare = async (ticket: string) => {
   if (!ticket) return null;
